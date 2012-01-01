c9a663b98d4de41696d3c522e5be4892396f73a9
tulius <tobifleig@gmail.com>
2012-01-02 00:03:01 +0100

    Engine zeigt Fähigkeiten an

--- src/de/_13ducks/spacebatz/client/graphics/Engine.java
+++ src/de/_13ducks/spacebatz/client/graphics/Engine.java
@@ -2,8 +2,11 @@ package de._13ducks.spacebatz.client.graphics;
 
 import java.lang.reflect.Field;
 import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextCapabilities;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.GLContext;
 
 /**
  * Kern der Grafikengine. Startet die Grafikausgabe
@@ -33,6 +36,28 @@ public class Engine {
         try {
             Display.setDisplayMode(new DisplayMode(800, 600));
             Display.create();
+            ContextCapabilities con = GLContext.getCapabilities();
+            boolean FBOEnabled = con.GL_EXT_framebuffer_object;
+            System.out.println("OpenGL Major Version Support:");
+            System.out.println("OpenGL 1.1 Support: " + con.OpenGL11);
+            System.out.println("OpenGL 1.2 Support: " + con.OpenGL12);
+            System.out.println("OpenGL 1.3 Support: " + con.OpenGL13);
+            System.out.println("OpenGL 1.4 Support: " + con.OpenGL14);
+            System.out.println("OpenGL 1.5 Support: " + con.OpenGL15);
+            System.out.println("OpenGL 2.0 Support: " + con.OpenGL20);
+            System.out.println("OpenGL 2.1 Support: " + con.OpenGL21);
+            System.out.println("OpenGL 3.0 Support: " + con.OpenGL30);
+            System.out.println("OpenGL 3.1 Support: " + con.OpenGL31);
+            System.out.println("OpenGL 3.2 Support: " + con.OpenGL32);
+            System.out.println("OpenGL 3.3 Support: " + con.OpenGL33);
+            System.out.println("OpenGL 4.0 Support: " + con.OpenGL40);
+            System.out.println("OpenGL 4.1 Support: " + con.OpenGL41);
+            System.out.println("OpenGL 4.2 Support: " + con.OpenGL42);
+            System.out.println("---------------------------------------------");
+            System.out.println("Individual Feature support:");
+            System.out.println("FBO-Support: " + FBOEnabled);
+            System.out.println("VBO-Support: " + con.GL_ARB_vertex_buffer_object);
+            System.out.println("Max Number of VBO-Vertices: " + GL12.GL_MAX_ELEMENTS_VERTICES);
         } catch (LWJGLException ex) {
             ex.printStackTrace();
             return;
