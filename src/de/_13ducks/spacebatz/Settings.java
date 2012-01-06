4942e80715d331d3c720aec7463d655b1749703c
tulius <tobifleig@gmail.com>
2012-01-06 23:42:11 +0100

    Statisch erreichbare Settings, der Einfachheit halber

---
+++ src/de/_13ducks/spacebatz/Settings.java
@@ -0,0 +1,22 @@
+package de._13ducks.spacebatz;
+
+/**
+ * Spieleinstellungen, der Einfachheit halber hier statisch erreichbar.
+ * Achtung: Für Server und Client gleich!!
+ * @author Tobias Fleig <tobifleig@googlemail.com>
+ */
+public final class Settings {
+    
+    /**
+     * Private, da Utility-Class
+     */
+    private Settings() {
+        
+    }
+    
+    public static final int CLIENT_GFX_RES_X = 800;
+    public static final int CLIENT_GFX_RES_Y = 600;
+    public static final int CLIENT_GFX_TILESIZE = 16;
+    public static final int CLIENT_GFX_TILEZOOM = 2;
+
+}
