f825e42375dcba93c7791f5f7f81fca54a5cd1b2
tulius <tobifleig@gmail.com>
2012-01-06 23:05:48 +0100

    Client ist jetzt main

--- src/de/_13ducks/spacebatz/client/Client.java
+++ src/de/_13ducks/spacebatz/client/Client.java
@@ -1,9 +1,16 @@
 package de._13ducks.spacebatz.client;
 
+import de._13ducks.spacebatz.client.graphics.Engine;
+
 /**
  * Die Hauptklasse des Clients
+ *
  * @author michael
  */
 public class Client {
-    
+
+    // Einstiegspunkt:
+    public static void main(String[] args) {
+        new Engine().start();
+    }
 }
