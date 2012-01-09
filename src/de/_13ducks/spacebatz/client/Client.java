7b0d31d15055eae4b7af501034f90ad8000bfa52
2nd <2ndCalc@13ducks.de>
2012-01-09 17:52:14 +0100

    Client rendert Bullets

--- src/de/_13ducks/spacebatz/client/Client.java
+++ src/de/_13ducks/spacebatz/client/Client.java
@@ -1,6 +1,7 @@
 package de._13ducks.spacebatz.client;
 
 import de._13ducks.spacebatz.client.graphics.Engine;
+import java.util.LinkedList;
 
 /**
  * Die Hauptklasse des Clients
@@ -13,11 +14,40 @@ public class Client {
      * Das derzeit laufende Level.
      */
     public static Level currentLevel;
-
+    /*
+     * aktueller Tick, wird einmal von Server empfangen werden...
+     */
+    private static int gametick;
+    /*
+     * List für alle aktuellen Bullets
+     */
+    private static LinkedList<Bullet> BulletList = new LinkedList<Bullet>();
+    
     // Einstiegspunkt:
     public static void main(String[] args) {
         //Neues Level erstellen:
         currentLevel = LevelGenerator.generateLevel(128, 128);
         new Engine().start();
+    }
+
+    /**
+     * @return the gametick
+     */
+    public static int getGametick() {
+        return gametick;
+    }
+
+    /**
+     * @param aGametick the gametick to set
+     */
+    public static void incrementGametick() {
+        gametick++;
+    }
+
+    /**
+     * @return the BulletList
+     */
+    public static LinkedList<Bullet> getBulletList() {
+        return BulletList;
     }
 }
