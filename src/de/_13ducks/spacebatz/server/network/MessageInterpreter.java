35b246894f751ba5e2d9ddf97d656022947daede
tulius <tobifleig@gmail.com>
2012-01-09 18:36:49 +0100

    MessageInterpreter und ServerNetwork weisen darauf hin (u.A. JavaDoc), was TCP-Signale sind und was UDP

--- src/de/_13ducks/spacebatz/server/network/MessageInterpreter.java
+++ src/de/_13ducks/spacebatz/server/network/MessageInterpreter.java
@@ -1,6 +1,5 @@
 package de._13ducks.spacebatz.server.network;
 
-import de._13ducks.spacebatz.server.SpacebatzServer;
 import de._13ducks.spacebatz.server.data.Client;
 
 
@@ -11,26 +10,16 @@ import de._13ducks.spacebatz.server.data.Client;
 public class MessageInterpreter {
 
     /**
-     * interpretiert eine Nachricht vion einem Client
+     * Interpretiert eine TCP Nachricht von einem Client.
      * @param message die Nachricht als byte-array
      */
-    public void interpretMessage(byte message[], Client sender){
-        System.out.print("Message received: ");
+    public void interpretMessage(byte message[], Client sender) {
+        System.out.print("TCP received: ");
         for(int i=0; i<message.length; i++){
             System.out.print((int)message[i]);
         }
         System.out.print("\n");
         
-        
-        for(int i=0; i< SpacebatzServer.game.chars.size(); i++){
-            if((byte)SpacebatzServer.game.chars.get(i).id == message[0])
-            {
-                SpacebatzServer.game.chars.get(i).posX = (int)message[1];
-                SpacebatzServer.game.chars.get(i).posY = (int)message[2];
-            }
-        }
-        
-        
-        
+        //TODO: Nachricht interpretieren...
     }
 }
