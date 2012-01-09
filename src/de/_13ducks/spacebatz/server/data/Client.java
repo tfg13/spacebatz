bbdacfe9edc31fb0d072360e4195d13f83aa57dc
tulius <tobifleig@gmail.com>
2012-01-09 18:37:51 +0100

    Server-Playerklasser erbt von Char, Client hat Referenz auf Serverklasse

--- src/de/_13ducks/spacebatz/server/data/Client.java
+++ src/de/_13ducks/spacebatz/server/data/Client.java
@@ -4,19 +4,32 @@ import de._13ducks.spacebatz.server.network.ServerNetworkConnection;
 
 /**
  * Diese Klasse stellt einen Client dar
+ *
  * @author michael
  */
 public class Client {
-    
+
     /**
      * The NetworkConection of this Client
      */
     private ServerNetworkConnection connection;
-    
+    /**
+     * Das zugehörige Player-Objekt
+     */
+    private Player player;
+
     /**
      * gibt die Netzwerkverbindung des Clients zurück
      */
-    public ServerNetworkConnection getNetworkConnection(){
-     return connection;    
+    public ServerNetworkConnection getNetworkConnection() {
+        return connection;
+    }
+
+    void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
     }
 }
---
+++ src/de/_13ducks/spacebatz/server/data/Player.java
@@ -0,0 +1,21 @@
+package de._13ducks.spacebatz.server.data;
+
+/**
+ *
+ * @author Tobias Fleig <tobifleig@googlemail.com>
+ */
+public class Player extends Char {
+
+    /**
+     * Erzeugt einen neuen Player für den angegebenen Client. Dieser Player wird auch beim Client registriert. Es kann nur einen Player pro Client geben.
+     *
+     * @param x Startkoordinate X
+     * @param y Startkoordinate Y
+     * @param id netID, nicht mehr änderbar.
+     * @param client der Client, dem dieser Player gehören soll.
+     */
+    public Player(double x, double y, int id, Client client) {
+        super(x, y, id);
+        client.setPlayer(this);
+    }
+}
