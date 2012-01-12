e6a37437c8c0236b0d1ced496be4538940d60765
tulius <tobifleig@gmail.com>
2012-01-12 17:40:12 +0100

    ServerPlayer speichert den Tick, bei dem zuletzt die Position vom Client aktualisiert wurde

--- src/de/_13ducks/spacebatz/server/data/Player.java
+++ src/de/_13ducks/spacebatz/server/data/Player.java
@@ -5,6 +5,11 @@ package de._13ducks.spacebatz.server.data;
  * @author Tobias Fleig <tobifleig@googlemail.com>
  */
 public class Player extends Char {
+    
+    /**
+     * Bei welchem Tick zuletzt eine Positionsänderung vom Client empfangen wurde.
+     */
+    private int lastPosTick;
 
     /**
      * Erzeugt einen neuen Player für den angegebenen Client. Dieser Player wird auch beim Client registriert. Es kann nur einen Player pro Client geben.
