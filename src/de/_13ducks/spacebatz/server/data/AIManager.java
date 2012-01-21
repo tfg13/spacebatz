4bd0903a8e4d9338e33fb614e02a55f3610683be
michael <mekhar@gmx.de>
2012-01-21 23:26:41 +0100

    KI funktioniert jetzt

--- src/de/_13ducks/spacebatz/server/data/AIManager.java
+++ src/de/_13ducks/spacebatz/server/data/AIManager.java
@@ -27,16 +27,26 @@ public class AIManager {
 
                 // Hat der Mob ein Ziel?
                 if (mob.getMyTarget() == null) {
+                    // wenn er kein Ziel hat sucht er ob eines in dwer Nähe ist:
                     for (int j = 0; j < Server.game.chars.size(); j++) {
-                        Char theChar = Server.game.chars.get(j);
-                        if (Settings.MOB_AGGRO_RANGE < Distance.getDistance(mob.getX(), mob.getY(), theChar.getX(), theChar.getY())) {
-                            mob.setMyTarget(theChar);
+                        if (Server.game.chars.get(j) instanceof Player) {
+                            Char theChar = Server.game.chars.get(j);
+                            if (Settings.MOB_AGGRO_RANGE > Distance.getDistance(mob.getX(), mob.getY(), theChar.getX(), theChar.getY())) {
+                                mob.setMyTarget(theChar);
+                            }
                         }
                     }
                 } else {
-                    
-                    mob.setVector(mob.getMyTarget().posX - mob.getX(), mob.getMyTarget().posY - mob.getY());
-                    
+                    // wenn er eins hat schaut er ob es noch in reichweite ist:
+                    if (Settings.MOB_AGGRO_RANGE < Distance.getDistance(mob.getX(), mob.getY(), mob.getMyTarget().getX(), mob.getMyTarget().getY())) {
+                        mob.setMyTarget(null);
+                        mob.stopMovement();
+                    } else {
+                        // wenn es in reichweite ist  hinbewegen:
+                        mob.setVector(mob.getMyTarget().posX - mob.getX(), mob.getMyTarget().posY - mob.getY());
+                    }
+
+
                 }
             }
 
