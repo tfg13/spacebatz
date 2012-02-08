bda47c3686bd0ea8b16d0a0fa9fbcaa37b90c90d
2nd <2ndCalc@13ducks.de>
2012-02-08 18:35:15 +0100

    Server nennt Client und das Item das er anlegen will richtig

--- src/de/_13ducks/spacebatz/server/network/ServerMessageInterpreter.java
+++ src/de/_13ducks/spacebatz/server/network/ServerMessageInterpreter.java
@@ -59,9 +59,14 @@ public class ServerMessageInterpreter {
             case Settings.NET_TCP_CMD_EQUIP_ITEM:
                 int netID = Bits.getInt(message, 0);
                 int slot = Bits.getInt(message, 4);
-                Item item = (Item) Server.game.getItemMap().get(netID);
+                Item item = null;
+                for (int i = 0; i < sender.getInventory().getItems().size(); i++) {
+                    if (sender.getInventory().getItems().get(i).netID == netID) {
+                        item = sender.getInventory().getItems().get(i);
+                    }
+                }
                 sender.getEquippedItems()[slot] = item;
-                System.out.println("Der Client will Item equippen!" + item.stats.itemStats.get("name"));
+                System.out.println("Der Client " + sender.clientID + " will Item equippen: " + item.stats.itemStats.get("name"));
                 break;
         }
     }
