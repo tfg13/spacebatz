37573a2187921e463dce908d5d3d34cffbb7b911
michael <mekhar@gmx.de>
2012-03-06 15:36:22 +0100

    AI-Manager kann jetzt Gegner mit unterschiedlichem Verhalten berechnen

--- src/de/_13ducks/spacebatz/server/gamelogic/AIManager.java
+++ src/de/_13ducks/spacebatz/server/gamelogic/AIManager.java
@@ -20,6 +20,15 @@ import java.util.Collection;
 public class AIManager {
 
     /**
+     * Konstante für für Enemys die nichts machen
+     */
+    public static final int AITYPE_NONE = 0;
+    /**
+     * Konstante für Standard Mobverhalten (Bei Sichtkontakt zum Spieler hinrennen und ihn verfolgen)
+     */
+    public static final int AITYPE_STANDARD = 1;
+
+    /**
      * Berechnet das Verhalten der mobs.
      *
      * @param allList die Liste aller mobs deren Verhalten berechnet werden soll
@@ -28,37 +37,57 @@ public class AIManager {
         for (Sync e : allList) {
             if (e instanceof Enemy) {
                 Enemy mob = (Enemy) e;
-                // Hat der Mob ein Ziel?
-                if (mob.getMyTarget() == null) {
-                    // wenn er kein Ziel hat sucht er ob eines in dwer Nähe ist:
-                    for (Client client : Server.game.clients.values()) {
-                        Player player = client.getPlayer();
-                        if (mob.getSightrange() > Distance.getDistance(mob.getX(), mob.getY(), player.getX(), player.getY())) {
-                            mob.setMyTarget(player);
-                        }
-                    }
-                } else {
-                    // wenn er eins hat schaut er ob es noch in reichweite ist:
-                    if (mob.getSightrange() * 2 < Distance.getDistance(mob.getX(), mob.getY(), mob.getMyTarget().getX(), mob.getMyTarget().getY())) {
-                        mob.setMyTarget(null);
-                        mob.stopMovement();
-                    } else {
-                        // wenn es in reichweite ist  hinbewegen:
-                        double vecX = mob.getMyTarget().getX() - mob.getX();
-                        double vecY = mob.getMyTarget().getY() - mob.getY();
 
-                        // Sicher gehen, dass die Vektoren nicht 0 sind:
-                        if (vecX == 0.0) {
-                            vecX = 0.1;
-                        }
-                        if (vecY == 0.0) {
-                            vecY = 0.1;
-                        }
-
-                        mob.setVector(vecX, vecY);
-                    }
+                switch (mob.getAiType()) {
+                    case AITYPE_NONE:
+                        break;
+                    case AITYPE_STANDARD:
+                        computeStandardMobBehaviour(mob);
+                        break;
                 }
+
+
             }
         }
+    }
+
+    /**
+     * Berechnet das Standard-MobverhaltenX
+     *
+     * @param mob der Enemy für den das Standardverhalten berechnet werde nsoll
+     */
+    private static void computeStandardMobBehaviour(Enemy mob) {
+
+        // Hat der Mob ein Ziel?
+        if (mob.getMyTarget() == null) {
+            // wenn er kein Ziel hat sucht er ob eines in dwer Nähe ist:
+            for (Client client : Server.game.clients.values()) {
+                Player player = client.getPlayer();
+                if (mob.getSightrange() > Distance.getDistance(mob.getX(), mob.getY(), player.getX(), player.getY())) {
+                    mob.setMyTarget(player);
+                }
+            }
+        } else {
+            // wenn er eins hat schaut er ob es noch in reichweite ist:
+            if (mob.getSightrange() * 2 < Distance.getDistance(mob.getX(), mob.getY(), mob.getMyTarget().getX(), mob.getMyTarget().getY())) {
+                mob.setMyTarget(null);
+                mob.stopMovement();
+            } else {
+                // wenn es in reichweite ist  hinbewegen:
+                double vecX = mob.getMyTarget().getX() - mob.getX();
+                double vecY = mob.getMyTarget().getY() - mob.getY();
+
+                // Sicher gehen, dass die Vektoren nicht 0 sind:
+                if (vecX == 0.0) {
+                    vecX = 0.1;
+                }
+                if (vecY == 0.0) {
+                    vecY = 0.1;
+                }
+
+                mob.setVector(vecX, vecY);
+            }
+        }
+
     }
 }
