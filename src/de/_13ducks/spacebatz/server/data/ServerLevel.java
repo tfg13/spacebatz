e10a93cac75e381dc7bb121ff9694dfa551950b0
michael <mekhar@gmx.de>
2012-03-07 13:05:19 +0100

    EnemySpawner eingebaut

--- src/de/_13ducks/spacebatz/server/data/ServerLevel.java
+++ src/de/_13ducks/spacebatz/server/data/ServerLevel.java
@@ -1,18 +1,35 @@
 package de._13ducks.spacebatz.server.data;
 
 import de._13ducks.spacebatz.shared.Level;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
- * Das Level des Servers
- * erweitert das ClientLevel um einige Infos die nur der Server braucht
+ * Das Level des Servers erweitert das ClientLevel um einige Infos die nur der Server braucht
+ *
  * @author michael
  */
-public class ServerLevel extends Level  {
+public class ServerLevel extends Level {
+
+    /**
+     * Die Liste mit Spawnern
+     */
+    private ArrayList<EnemySpawner> enemySpawners;
 
     /**
      * Konstruktor
      */
     public ServerLevel(int xSize, int ySize) {
         super(xSize, ySize);
+        enemySpawners = new ArrayList<>();
+    }
+
+    /**
+     * Gibt die Liste mit Spawnern zurück
+     *
+     * @return die Liste der Spawner
+     */
+    public ArrayList<EnemySpawner> getEnemySpawners() {
+        return enemySpawners;
     }
 }
