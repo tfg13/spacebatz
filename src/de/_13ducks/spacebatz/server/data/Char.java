5edb072140bd357e9631833953761dbb011bbaa5
michael <mekhar@gmx.de>
2012-03-31 13:11:53 +0200

    Jeder Char hat eine Liste von Fähigkeiten, die er einsetzen kann.

--- src/de/_13ducks/spacebatz/server/data/Char.java
+++ src/de/_13ducks/spacebatz/server/data/Char.java
@@ -11,6 +11,8 @@
 package de._13ducks.spacebatz.server.data;
 
 import de._13ducks.spacebatz.Settings;
+import de._13ducks.spacebatz.server.data.abilities.Ability;
+import java.util.HashMap;
 
 /**
  * Ein bewegliches Objekt. (z.B. ein Spieler, Mob etc)
@@ -59,6 +61,11 @@ public abstract class Char extends Entity {
      * Die Größe des Chars (für Kollision)
      */
     private double size;
+    /**
+     * Die Liste aller Fähigkeiten, die der Char hat.
+     * Chars können höchstens eine Fähigkeit pro FähigkeitenTyp haben!
+     */
+    private HashMap<Integer, Ability> abilities;
 
     /**
      * Konstruktor, erstellt einen neuen Char
@@ -76,6 +83,92 @@ public abstract class Char extends Entity {
         this.range = 10.0;
         this.sightrange = 10;
         this.attackcooldown = 60;
+        abilities = new HashMap<>();
+    }
+
+    /**
+     * Benutzt eine Fähigkeit dieses Chars.
+     *
+     * @param ability die ID der Ability
+     */
+    public void useAbility(int ability) {
+        if (!abilities.containsKey(ability)) {
+            throw new IllegalArgumentException("No such Ability!");
+        } else {
+            abilities.get(ability).use();
+        }
+    }
+
+    /**
+     * Benutzt eine Fähigkeit dieses Chars auf eine Position.
+     *
+     * @param ability die ID der Ability
+     * @param x X-Koordinate der Zielposition
+     * @param y Y-Koordinate der Zielposition
+     */
+    public void useAbilityOnPosition(int ability, double x, double y) {
+        if (!abilities.containsKey(ability)) {
+            throw new IllegalArgumentException("No such Ability!");
+        } else {
+            abilities.get(ability).useOnPosition(x, y);
+        }
+    }
+
+    /**
+     * Benutzt eine Fähigkeit dieses Chars in eine bestimmte Richtung.
+     *
+     * @param ability die ID der Ability
+     * @param angle die Richtung in die die Fähigkeit benutzt werden soll
+     */
+    public void useAbilityInAngle(int ability, double angle) {
+        if (!abilities.containsKey(ability)) {
+            throw new IllegalArgumentException("No such Ability!");
+        } else {
+            abilities.get(ability).useInAngle(range);
+        }
+    }
+
+    /**
+     * Benutzt eine Fähigkeit dieses Chars auf eine Zielentity.
+     *
+     * @param ability die ID der Ability
+     * @param target das Ziel, auf das fie Fähigkeit benutzt werden soll
+     */
+    public void useAbilityOnTarget(int ability, Entity target) {
+        if (!abilities.containsKey(ability)) {
+            throw new IllegalArgumentException("No such Ability!");
+        } else {
+            abilities.get(ability).useOnTarget(target);
+        }
+    }
+
+    /**
+     * Gibt dem Char eine neue Fähigkeit.
+     *
+     * @param id der Typ der neuen Fähigkeit
+     * @param ability die neue Fähigkeit
+     */
+    public void addAbility(int id, Ability ability) {
+        if (abilities.containsKey(id)) {
+            throw new IllegalArgumentException("There is already a ability with that id!");
+        } else {
+            abilities.put(id, ability);
+            ability.setOwner(this);
+        }
+    }
+
+    /**
+     * Entfernt die Fähigkeit eines Typs des Chars.
+     *
+     * @param id der Typ der Fähigkeit die entfernt werden soll
+     */
+    public void removeAbility(int id) {
+        if (!abilities.containsKey(id)) {
+            throw new IllegalArgumentException("Char has no such Ability!");
+        } else {
+            abilities.get(id).setOwner(null);
+            abilities.remove(id);
+        }
     }
 
     /**
