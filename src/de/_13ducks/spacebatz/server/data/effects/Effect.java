f3a401e3c74a2892cb2bf31eba5ee96e410963e9
michael <mekhar@gmx.de>
2012-03-31 13:10:00 +0200

    Effekte umgebaut und demo-effekte entfernt

--- src/de/_13ducks/spacebatz/server/data/effects/Effect.java
+++ src/de/_13ducks/spacebatz/server/data/effects/Effect.java
@@ -1,11 +1,37 @@
 package de._13ducks.spacebatz.server.data.effects;
 
+import de._13ducks.spacebatz.server.data.Char;
+
 /**
- * Interface für alle Arten von Effekten
- *
- * Dieses Interface sorgt dafür, dass sowohl AreaEffect als auch CharEffect als Effekte behandelt werden können
+ * Superklasse für Effekte.
  *
  * @author michael
  */
-public interface Effect {
+public abstract class Effect {
+
+    /**
+     * Wenn ein Char von diesem Effekt beeinflust wird, ruft er regelmäßig diese Funktion auf.
+     */
+    public abstract void tick();
+
+    /**
+     * Wird aufgerufen, wenn dieser Effekt auf einen Char angewandt wird
+     *
+     * @param affectedChar der Char, auf den der Effekt angewandt wird
+     */
+    public abstract void applyToChar(Char affectedChar);
+
+    /**
+     * Löst diesen Effekt an einer Position aus.
+     * Kann z.B. für AoE-Effekte verwendet werden
+     *
+     * @param x X-Koordinate der Position
+     * @param y Y-Koordinate der Position
+     */
+    public abstract void applyToPosition(double x, double y);
+
+    /**
+     * Entfernt den Effekt wieder
+     */
+    public abstract void remove();
 }
