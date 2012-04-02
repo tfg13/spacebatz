4edd635dcb7acbb5a84bf9272e4fcadaa44d7f66
michael <mekhar@gmx.de>
2012-04-02 17:30:44 +0200

    Inventar-Klasse wieder eingebaut

---
+++ src/de/_13ducks/spacebatz/server/data/Inventory.java
@@ -0,0 +1,73 @@
+package de._13ducks.spacebatz.server.data;
+
+/*
+ * Copyright 2011, 2012:
+ * Tobias Fleig (tobifleig[AT]googlemail[DOT]com)
+ * Michael Haas (mekhar[AT]gmx[DOT]de)
+ * Johannes Kattinger (johanneskattinger[AT]gmx[DOT]de
+ *
+ * - All rights reserved -
+ *
+ * 13ducks PROPRIETARY/CONFIDENTIAL - do not distribute
+ */
+import de._13ducks.spacebatz.shared.Item;
+import java.util.HashMap;
+
+/**
+ * Das Inventar eines Clients, serverseitig
+ *
+ * @author der Nikolaus
+ */
+public class Inventory {
+
+    /**
+     * Items im Inventar
+     */
+    private HashMap<Integer, Item> items;
+    /**
+     * Wieviel Geld im Inventar ist
+     */
+    private int money;
+
+    public Inventory() {
+        items = new HashMap<>();
+    }
+
+    /**
+     * @return the items
+     */
+    public HashMap<Integer, Item> getItems() {
+        return items;
+    }
+
+    /**
+     * @param items the items to set
+     */
+    public void setItems(HashMap<Integer, Item> items) {
+        this.items = items;
+    }
+
+    /**
+     * @param items the items to add
+     */
+    public void putItem(int netID, Item item) {
+        this.items.put(netID, item);
+        if (item.getProperty("itemclass") == 0) {
+            setMoney(getMoney() + (int) item.getProperty("amount"));
+        }
+    }
+
+    /**
+     * @return the money
+     */
+    public int getMoney() {
+        return money;
+    }
+
+    /**
+     * @param money the money to set
+     */
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
