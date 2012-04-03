df04cac6836df50691ebfa81b6d4fe798ebbda0f
michael <mekhar@gmx.de>
2012-04-03 17:04:58 +0200

    der Hitmanager berechnet Treffer, Schaden, Effeklte, Drops etc.

---
+++ src/de/_13ducks/spacebatz/server/gamelogic/HitManager.java
@@ -0,0 +1,42 @@
+package de._13ducks.spacebatz.server.gamelogic;
+
+import de._13ducks.spacebatz.server.Server;
+import de._13ducks.spacebatz.server.data.Bullet;
+import de._13ducks.spacebatz.server.data.Char;
+
+/**
+ * Berechnet alles, was passiert wenn zwei Dinge kollidieren (zb Bullets mit Chars).
+ * Kümmert sich zB um das löschen von Bullets, Schaden, Effekte, etc...
+ *
+ * @author michael
+ */
+public class HitManager {
+
+    /**
+     * Berechnet Kollisionen zwischen Bullets und Chars.
+     *
+     * @param character der Char der mit dem Bullet kollidiert
+     * @param bullet das Bullet das mit dem Char kollidiert
+     */
+    public static void charBulletCollision(Char character, Bullet bullet) {
+        // abbrechen, wenn der Char das Bullet selber erzeugt hat:
+        if (bullet.getOwner().equals(character)) {
+            return;
+        }
+        
+        character.decrementProperty("hitpoints", 10);
+        if (character.getProperty("hitpoints") < 0) {
+            Server.game.netIDMap.remove(character.netID);
+            Server.entityMap.removeEntity(character);
+            DropManager.dropItem(character.getX(), character.getY(), 2);
+        }
+        Server.game.netIDMap.remove(bullet.netID);
+        Server.entityMap.removeEntity(bullet);
+    }
+
+    /**
+     * Berechnet Kollision eines Bullets mit einer Wand.
+     */
+    public static void bulletWallCollision() {
+    }
+}
