67169127ac4f6c24452ce9633e0b9bc27e389f4e
michael <mekhar@gmx.de>
2012-04-04 12:51:27 +0200

    Waffen können jetzt benutzt werden.

--- src/de/_13ducks/spacebatz/shared/Weapon.java
+++ src/de/_13ducks/spacebatz/shared/Weapon.java
@@ -1,5 +1,6 @@
 package de._13ducks.spacebatz.shared;
 
+import de._13ducks.spacebatz.server.data.Entity;
 import de._13ducks.spacebatz.server.data.abilities.Ability;
 
 /**
@@ -30,11 +31,47 @@ public class Weapon extends Item {
     }
 
     /**
-     * Gibt die Fähigkeit dieser Waffe zurück
-     *
-     * @return die Fähigkeit dieser Waffe
+     * Benutzt die Waffe.
      */
-    public Ability getAbility() {
-        return weaponAbility;
+    public void use() {
+        weaponAbility.use();
+    }
+
+    /**
+     * Benutzt die Waffe auf eine Position
+     *
+     * @param targetX die X-Koordinate des Ziels
+     * @param targetY die Y-Koordinate des Ziels
+     */
+    public void useOnPosition(double targetX, double targetY) {
+        weaponAbility.useOnPosition(targetX, targetY);
+    }
+
+    /**
+     * Benutzt die Waffe auf eine Entity
+     *
+     * @param target die Entity auf die die Waffe benutzt werden soll
+     */
+    public void useOnTarget(Entity target) {
+        weaponAbility.useOnTarget(target);
+    }
+
+    /**
+     * Benutzt die Waffe in eine Richtung
+     *
+     * @param angle der Winkel, in den die Waffe benutzt werden soll
+     */
+    public void useInAngle(double angle) {
+        weaponAbility.useInAngle(angle);
+    }
+
+    /**
+     * Gibt true zurück, wenn alle Bedingungen erfüllt sind um diese Waffe zu verwenden.
+     * (z.B. Cooldown, Energiekosten, ...)
+     *
+     * @return true, wenn die Waffe benutzt werden kann
+     */
+    public boolean isReady() {
+        return weaponAbility.isReady();
     }
 }
