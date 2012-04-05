98f92e6b5ff4522e81fea1c760410082c9dd55a9
michael <mekhar@gmx.de>
2012-04-05 12:31:19 +0200

    Bullets machen Schaden.

--- src/de/_13ducks/spacebatz/server/data/abilities/FireBulletAbility.java
+++ src/de/_13ducks/spacebatz/server/data/abilities/FireBulletAbility.java
@@ -3,6 +3,7 @@ package de._13ducks.spacebatz.server.data.abilities;
 import de._13ducks.spacebatz.server.Server;
 import de._13ducks.spacebatz.server.data.Bullet;
 import de._13ducks.spacebatz.server.data.Entity;
+import de._13ducks.spacebatz.server.data.effects.TrueDamageEffect;
 import de._13ducks.spacebatz.shared.Properties;
 import java.util.Random;
 
@@ -22,7 +23,7 @@ public class FireBulletAbility extends Ability {
     private double bulletspeed;
     private double spread;
     private double explosionradius;
-
+    
     public FireBulletAbility(double damage, double attackspeed, double range, int bulletpic, double bulletspeed, double spread, double explosionradius) {
         this.damage = damage;
         this.attackspeed = attackspeed;
@@ -32,22 +33,22 @@ public class FireBulletAbility extends Ability {
         this.spread = spread;
         this.explosionradius = explosionradius;
     }
-
+    
     @Override
     public void use() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    
     @Override
     public void useOnPosition(double x, double y) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    
     @Override
     public void useOnTarget(Entity target) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    
     @Override
     public void useInAngle(double angle) {
         if (owner.getAttackCooldownTick() <= Server.game.getTick()) {
@@ -55,18 +56,19 @@ public class FireBulletAbility extends Ability {
             Random random = new Random();
             angle += random.nextGaussian() * spread;
             int lifetime = (int) (range / bulletspeed);
-
+            
             Bullet bullet = new Bullet(Server.game.getTick(), lifetime, owner.getX(), owner.getY(), angle, bulletspeed, bulletpic, Server.game.newNetID(), owner);
+            bullet.addEffect(new TrueDamageEffect(1));
             Server.game.netIDMap.put(bullet.netID, bullet);
         }
     }
-
+    
     @Override
     public boolean isReady() {
         // IMMER BEREIT!
         return true;
     }
-
+    
     @Override
     public void refreshProperties(Properties properties) {
         // Werte des Trägers werden ignoriert.
