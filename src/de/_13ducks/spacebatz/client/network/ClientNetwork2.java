3e4ae6b60825fb38523c3dedfe8b7139a9e774a9
tulius <tobifleig@gmail.com>
2012-05-18 19:06:01 +0200

    NET: Client-Netzwerkmodul angefangen, kann sich zum Server verbinden

---
+++ src/de/_13ducks/spacebatz/client/network/ClientNetwork2.java
@@ -0,0 +1,133 @@
+package de._13ducks.spacebatz.client.network;
+
+import de._13ducks.spacebatz.util.Bits;
+import java.io.IOException;
+import java.net.*;
+
+/**
+ * Die Client-Seite des neuen Netzwerksystems
+ *
+ * @author Tobias Fleig <tobifleig@googlemail.com>
+ */
+public class ClientNetwork2 {
+
+    /**
+     * Sagt ob wir zu einem Server verbunden sind, und daher Daten senden und empfangen können.
+     */
+    private boolean connected = false;
+    /**
+     * Thread, der auf UDP-Pakete lauscht.
+     */
+    private Thread thread;
+    /**
+     * Das verwendete Socket.
+     */
+    private DatagramSocket socket;
+
+    /**
+     * Erzeugt ein neues Netzwerkmodul.
+     */
+    public ClientNetwork2() {
+    }
+
+    /**
+     * Weist das Netzwerksystem an, sich zur Zieladdresse/Port zu verbinden.
+     * Liefert true bei erfolgreichem Verbindungsaufbau.
+     * Anschließend ist der Setup des Netzwerksystems abgeschlossen und es kann verwendet werden.
+     * Wenn false returned wurde, kann diese Methode für weitere Versuche erneut aufgerufen werden.
+     * Diese Methode blockt, bis der Server geantwortet hat. (Maximal 5 Sekunden)
+     *
+     * @param targetAddress die Zieladdresse
+     * @param port der Ziel-Port
+     * @return true, wenn erfolgreich, sonst false
+     */
+    public synchronized boolean connect(InetAddress targetAddress, int port) {
+	try {
+	    socket = new DatagramSocket();
+	} catch (SocketException ex) {
+	    return false;
+	}
+	try {
+	    // Initialize-Paket an den Server schicken:
+	    byte[] data = new byte[4];
+	    Bits.putInt(data, 0, socket.getLocalPort());
+	    data[0] = (byte) (1 << 6); // NETMODE auf noClient, connect
+	    DatagramPacket packet = new DatagramPacket(data, data.length, targetAddress, port);
+	    // Antwort-Packet
+	    byte[] ansData = new byte[2];
+	    DatagramPacket ansPacket = new DatagramPacket(ansData, ansData.length);
+	    socket.setSoTimeout(50000);
+	    socket.send(packet);
+	    try {
+		socket.receive(ansPacket);
+		socket.setSoTimeout(-1);
+	    } catch (SocketTimeoutException timeoutEx) {
+		// Timeout, ging nicht, Ende.
+		socket.close();
+		return false;
+	    }
+	    // Antwort auswerten (erstes Bit):
+	    if ((ansData[0] & 0x80) != 0) {
+		// Verbindung ok, Parameter auslesen.
+		int nextTick = ansData[0] & 0x7F;
+		int clientID = ansData[1];
+		System.out.println("INFO: NET: Connection establised. ClientID " + clientID + ", nextTick " + nextTick);
+		initializeReceiver();
+		return true;
+	    } else {
+		System.out.println("Connecting failed. Server rejected request. Reason: " + (ansData[0] & 0x7F));
+		socket.close();
+		return false;
+	    }
+	} catch (IOException ex) {
+	    socket.close();
+	    return false;
+	}
+    }
+
+    private void initializeReceiver() {
+	thread = new Thread(new Runnable() {
+
+	    @Override
+	    public void run() {
+		try {
+		    while (true) {
+			DatagramPacket pack = new DatagramPacket(new byte[512], 512);
+			// blockt
+			socket.receive(pack);
+			/*
+			 * TODO: Hier weitermachen, Daten queuen.
+			 */
+		    }
+		} catch (IOException ex) {
+		    System.out.println("ERROR: NET: Receiving data failed. Details:");
+		    ex.printStackTrace();
+		}
+	    }
+	}, "CNET_RECEIVE");
+	thread.setDaemon(true);
+	thread.start();
+    }
+
+    /**
+     * Muss zu Anfang jedes Ticks aufgerufen werden, verarbeitet Befehle vom Server.
+     * Darf erst nach connect aufgerufen werden.
+     */
+    public void inTick() {
+	if (connected) {
+	} else {
+	    System.out.println("ERROR: Cannot read inputData, not connected!");
+	}
+    }
+
+    /**
+     * Muss zu Ende jedes Ticks aufgerufen werden, sendet Daten an den Server.
+     * Darf erst nach connect aufgerufen werden.
+     */
+    public void outTick() {
+	if (connected) {
+	} else {
+	    System.out.println("ERROR: Cannot send data, not connected!");
+	}
+    }
+}
