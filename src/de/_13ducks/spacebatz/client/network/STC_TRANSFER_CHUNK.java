4d8bc52a393c3c57bc44e5b9f2e12a8ee31182f4
tulius <tobifleig@gmail.com>
2013-01-30 17:47:06 +0100

    Netzwerksystem überträgt neue Leveldaten

--- src/de/_13ducks/spacebatz/client/network/STC_TRANSFER_CHUNK.java
+++ src/de/_13ducks/spacebatz/client/network/STC_TRANSFER_CHUNK.java
@@ -12,6 +12,9 @@ public class STC_TRANSFER_CHUNK extends STCCommand {
     @Override
     public void execute(byte[] data) {
         int[][] ground = GameClient.currentLevel.ground;
+        int[][] top = GameClient.currentLevel.top;
+        int[][] dye_ground = GameClient.currentLevel.dye_ground;
+        int[][] dye_top = GameClient.currentLevel.dye_top;
         boolean[][] col = GameClient.currentLevel.getCollisionMap();
         byte[][] shadow = GameClient.currentLevel.shadow;
         int startX = Bits.getInt(data, 0) * 8;
@@ -21,6 +24,27 @@ public class STC_TRANSFER_CHUNK extends STCCommand {
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
                 ground[startX + x][startY + y] = Bits.getInt(data, dataIndex);
+                dataIndex += 4;
+            }
+        }
+        // Top
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                top[startX + x][startY + y] = Bits.getInt(data, dataIndex);
+                dataIndex += 4;
+            }
+        }
+        // dye_ground
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                dye_ground[startX + x][startY + y] = Bits.getInt(data, dataIndex);
+                dataIndex += 4;
+            }
+        }
+        // dye_top
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                dye_top[startX + x][startY + y] = Bits.getInt(data, dataIndex);
                 dataIndex += 4;
             }
         }
@@ -47,7 +71,7 @@ public class STC_TRANSFER_CHUNK extends STCCommand {
     @Override
     public int getSize(byte sizeData) {
         // Zwei mal Koordinaten, 64 ints texturen, 8 byte kollisionsdaten
-        return 64 * 4 + 8 + 8;
+        return 8 * 8 * 4 * 4 + 8 + 8;
     }
     
 }
