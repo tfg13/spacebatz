e54a1265c56e7e4f4ac6dd7efe6e9486459c5dfb
tulius <tobifleig@gmail.com>
2013-02-10 11:22:13 +0100

    Schatten-Shader: Experimente mit quadratischer Interpolation, funktioniert aber nicht richtig (alles auskommentiert, wird nur archiviert)

--- shaders/shadow.glsl
+++ shaders/shadow.glsl
@@ -11,5 +11,12 @@ void main(void)
     float midTop = mix(shadowLO, shadowRO, (gl_FragCoord.x - bx) / pixelPerSprite);
     float midBot = mix(shadowLU, shadowRU, (gl_FragCoord.x - bx) / pixelPerSprite);
     float midmid = mix(midBot, midTop, (gl_FragCoord.y - by) / pixelPerSprite);
+    //float factorX = (gl_FragCoord.x - bx) / pixelPerSprite;
+    //float factorY = (gl_FragCoord.y - by) / pixelPerSprite;
+    //float interpolateLU = sqrt(((1 - factorX) * (1 - factorX)) + ((1 - factorY) * (1 - factorY))) * shadowLU;
+    //float interpolateLO = sqrt(((1 - factorX) * (1 - factorX)) + (factorY * factorY)) * shadowLO;
+    //float interpolateRU = sqrt((factorX * factorX) + ((1 - factorY) * (1 - factorY))) * shadowRU;
+    //float interpolateRO = sqrt((factorX * factorX) + (factorY * factorY)) * shadowRO;
+    //gl_FragColor = vec4(0.0, 0.0, 0.0, (interpolateRO * interpolateRU * interpolateLO * interpolateLU));
     gl_FragColor = vec4(0.0, 0.0, 0.0, midmid);
 }
