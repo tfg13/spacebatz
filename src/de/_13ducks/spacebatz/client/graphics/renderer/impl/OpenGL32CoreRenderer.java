f8b0fc9b43a93ed84dc9b2ff932ce68c66d31132
tulius <tobifleig@gmail.com>
2013-05-30 21:42:44 +0200

    Default-Shader werden geladen und aktiviert

--- src/de/_13ducks/spacebatz/client/graphics/renderer/impl/OpenGL32CoreRenderer.java
+++ src/de/_13ducks/spacebatz/client/graphics/renderer/impl/OpenGL32CoreRenderer.java
@@ -2,6 +2,7 @@ package de._13ducks.spacebatz.client.graphics.renderer.impl;
 
 import de._13ducks.spacebatz.client.graphics.ShaderLoader;
 import de._13ducks.spacebatz.client.graphics.renderer.CoreRenderer;
+import org.lwjgl.opengl.GL20;
 
 /**
  * Neuer Haupt-Renderer, arbeitet ausschließlich mit OpenGL >=3.2,
@@ -11,13 +12,17 @@ import de._13ducks.spacebatz.client.graphics.renderer.CoreRenderer;
  */
 public class OpenGL32CoreRenderer extends CoreRenderer {
 
+    /**
+     * Enthält die ProgramIDs aller verwendeten Shader.
+     * Diese sind bereits fertig gelinkt etc. und können direkt verwendet werden.
+     */
     private int[] shader;
 
     @Override
     public void setupShaders() {
         // Shader laden, compilen, linken
         shader = ShaderLoader.load();
-
+        GL20.glUseProgram(shader[0]);
     }
 
     @Override
