33c0dadec0f93eba6e24439ab9864c6c6c927ad5
tulius <tobifleig@gmail.com>
2013-05-31 14:23:29 +0200

    Default-Shader ausgebrütet, Matrizen werden initialisiert

--- shaders/opengl32core/default.frag
+++ shaders/opengl32core/default.frag
@@ -1,5 +1,12 @@
 #version 150 core
 
+uniform sampler2D texture;
+
+in vec4 pass_Color;
+in vec4 pass_TextureCoord;
+
+out vec4 out_Color;
+
 void main() {
-    
+    out_Color = texture2D(texture, pass_TextureCoord);
 }
--- src/de/_13ducks/spacebatz/client/graphics/renderer/impl/OpenGL32CoreRenderer.java
+++ src/de/_13ducks/spacebatz/client/graphics/renderer/impl/OpenGL32CoreRenderer.java
@@ -2,7 +2,11 @@ package de._13ducks.spacebatz.client.graphics.renderer.impl;
 
 import de._13ducks.spacebatz.client.graphics.ShaderLoader;
 import de._13ducks.spacebatz.client.graphics.renderer.CoreRenderer;
+import de._13ducks.spacebatz.shared.DefaultSettings;
+import java.nio.FloatBuffer;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.util.vector.Matrix4f;
 
 /**
  * Neuer Haupt-Renderer, arbeitet ausschließlich mit OpenGL >=3.2,
@@ -23,6 +27,31 @@ public class OpenGL32CoreRenderer extends CoreRenderer {
         // Shader laden, compilen, linken
         shader = ShaderLoader.load();
         GL20.glUseProgram(shader[0]);
+        // Ortho-Projektionsmatrix aufmachen:
+        Matrix4f projection = new Matrix4f();
+        projection.m00 = 1f / 10f;
+        projection.m03 = -1f;
+        projection.m11 = (1f * DefaultSettings.CLIENT_GFX_RES_Y / DefaultSettings.CLIENT_GFX_RES_X) / 10f;
+        projection.m13 = -1f;
+        projection.m22 = -1f;
+        // View und Model erstmal Identity
+        Matrix4f view = new Matrix4f();
+        Matrix4f model = new Matrix4f();
+        // Projection und View ändern sich selten (und nicht während eines Frames), vormultiplizieren um dem Shader Zeit zu sparen.
+        Matrix4f projectionView = new Matrix4f();
+        Matrix4f.mul(projection, view, projectionView);
+        // Daten zum Shader hochladen
+        FloatBuffer matrix44Buffer = BufferUtils.createFloatBuffer(16);
+        // ProjectionView
+        projectionView.store(matrix44Buffer);
+        matrix44Buffer.flip();
+        int pvmloc = GL20.glGetUniformLocation(shader[0], "projectionViewM");
+        GL20.glUniformMatrix4(pvmloc, false, matrix44Buffer);
+        // Model
+        model.store(matrix44Buffer);
+        matrix44Buffer.flip();
+        int mmloc = GL20.glGetUniformLocation(shader[0], "modelM");
+        GL20.glUniformMatrix4(mmloc, false, matrix44Buffer);
     }
 
     @Override
