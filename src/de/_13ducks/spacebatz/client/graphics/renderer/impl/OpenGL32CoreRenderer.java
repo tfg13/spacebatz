cc812e8732e0cb128fe5bfa7a3fa2e5056bc1d2e
tulius <tobifleig@gmail.com>
2013-06-23 19:33:03 +0200

    Spieler wird wieder gedreht

--- src/de/_13ducks/spacebatz/client/graphics/renderer/impl/OpenGL32CoreRenderer.java
+++ src/de/_13ducks/spacebatz/client/graphics/renderer/impl/OpenGL32CoreRenderer.java
@@ -35,6 +35,10 @@ public class OpenGL32CoreRenderer extends CoreRenderer {
      */
     private static final int INDEX_VERT_PROJECTIONVIEW = 0;
     /**
+     * Index der Adresse der Modelmatrix im Array mit Uniformadressen.
+     */
+    private static final int INDEX_VERT_MODEL = 1;
+    /**
      * Enthält die ProgramIDs aller verwendeten Shader.
      * Diese sind bereits fertig gelinkt etc. und können direkt verwendet werden.
      */
@@ -42,7 +46,7 @@ public class OpenGL32CoreRenderer extends CoreRenderer {
     /**
      * Adressen der Uniforms.
      */
-    private int[] shaderUniformAdr = new int[1];
+    private int[] shaderUniformAdr = new int[2];
     /**
      * Projektions-Matrix, bleibt normalerweise immer gleich.
      */
@@ -96,8 +100,8 @@ public class OpenGL32CoreRenderer extends CoreRenderer {
         // Model
         model.store(matrix44Buffer);
         matrix44Buffer.flip();
-        int mmloc = GL20.glGetUniformLocation(shader[GraphicsEngine.SHADER_INDEX_GAME], "modelM");
-        GL20.glUniformMatrix4(mmloc, false, matrix44Buffer);
+        shaderUniformAdr[INDEX_VERT_MODEL] = GL20.glGetUniformLocation(shader[GraphicsEngine.SHADER_INDEX_GAME], "modelM");
+        GL20.glUniformMatrix4(shaderUniformAdr[INDEX_VERT_MODEL], false, matrix44Buffer);
     }
 
     @Override
@@ -145,21 +149,51 @@ public class OpenGL32CoreRenderer extends CoreRenderer {
         // Enemys
         updateVBOs();
         RenderUtils.getTextureByName("player.png").bind();
+        // Drehen:
+        pushRotMatrix((float) GameClient.player.getDir());
         testPlayer.render();
+        restoreRot();
 
     }
 
-    private void renderPlayer(int playerVAOAdr) {
-        // Render
-        GL30.glBindVertexArray(playerVAOAdr);
-        GL20.glEnableVertexAttribArray(0);
-        GL20.glEnableVertexAttribArray(1);
-        // Zeichnen
-        GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 6);
-        // Aufräumen
-        GL20.glDisableVertexAttribArray(0);
-        GL20.glDisableVertexAttribArray(1);
-        GL30.glBindVertexArray(0);
+    /**
+     * Schickt eine Rotationsmatrix an den Vertex-Shader.
+     *
+     * @param rotation rotation, in Bogenmaß, übliche zählweise
+     */
+    private void pushRotMatrix(float rotation) {
+        // Rotationszentrum verschieben:
+        Matrix4f trans = new Matrix4f();
+        trans.m30 = (float) GameClient.player.getSubtickedX(GraphicsEngine.SubTick.frozenSubTick);
+        trans.m31 = (float) GameClient.player.getSubtickedY(GraphicsEngine.SubTick.frozenSubTick);
+        // XY-Rotationsmatrix bauen
+        Matrix4f rot = new Matrix4f();
+        rot.m00 = (float) Math.cos(rotation);
+        rot.m11 = (float) Math.cos(rotation);
+        rot.m10 = (float) -Math.sin(rotation);
+        rot.m01 = (float) Math.sin(rotation);
+        // Multiplizieren
+        rot = Matrix4f.mul(trans, rot, null);
+        trans.m30 *= -1;
+        trans.m31 *= -1;
+        rot = Matrix4f.mul(rot, trans, null);
+        // Zur Grafikkarte hochladen:
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
+        rot.store(buffer);
+        buffer.flip();
+        GL20.glUniformMatrix4(shaderUniformAdr[INDEX_VERT_MODEL], false, buffer);
+    }
+
+    /**
+     * Setzt die Rotationsmatrix des Shaders zurück
+     */
+    private void restoreRot() {
+        // Identität laden:
+        Matrix4f identity = new Matrix4f();
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
+        identity.store(buffer);
+        buffer.flip();
+        GL20.glUniformMatrix4(shaderUniformAdr[INDEX_VERT_MODEL], false, buffer);
     }
 
     private void renderChunk(int chunkVAOAdr, int numberOfTriangles) {
